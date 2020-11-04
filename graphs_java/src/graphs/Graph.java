779eb998907aac323d8572f4969854fb87250e0b
protocolblack3302@gmail.com <protocolblack@Protocols-MacBook-Air.local>
2020-11-04 06:48:25 +0530

    kosaraju algorithm for strongly connected components added

--- graphs_java/src/graphs/Graph.java
+++ graphs_java/src/graphs/Graph.java
@@ -3,15 +3,28 @@ package graphs;
 import java.util.*;
 
 public class Graph {
+
     private int time;
-    private final HashMap<Node,LinkedList<Node>> adj;
-    private ArrayList<Node> index_nodes;  //these are key objects of hashmap we stored refrence of them here to use
+    private  HashMap<Node,LinkedList<Node>> adj;
+    private  HashMap<Node,LinkedList<Node>> transpose;
+    private ArrayList<Node> index_nodes;
+    private ArrayList<Node> index_nodes_transpose;//these are key objects of hashmap we stored refrence of them here to use
     private boolean cycle;
+    private LinkedList<Node> transpose_stack=new LinkedList<>();
+    
+    public enum TYPE{
+        DIRECTED,UNDIRECTED
+    }
+
+    private enum ON{
+        SIMPLE,
+        TRANSPOSE
+    }
 
     public static int distance(Graph g,int source,int destination){
         g.bfs(source);
-        return g.index_nodes.get(destination).distance;
 
+        return g.index_nodes.get(destination).distance;
     }
 
     private static class Node{
@@ -27,8 +40,7 @@ public class Graph {
     Node(int node_no){
         this.node_no=node_no;
         visited=false;
-        predecessor=null;
-        distance=0;
+
     }
 
     @Override
@@ -40,6 +52,8 @@ public class Graph {
         Node n=(Node) obj;
         return this.node_no == n.node_no;
 
+
+
     }
 
     @Override
@@ -47,52 +61,69 @@ public class Graph {
         return node_no;
     }
 
-
         @Override
         public String toString() {
             return String.valueOf(node_no);
         }
     }
 
-    public Graph(int vertices,int edges){
-        adj=new HashMap();
+    public Graph(int vertices,int edges,TYPE t){
+        adj=new HashMap<>();
         for(int i=0;i<vertices;i++){
             adj.put(new Node(i),new LinkedList<>());
         }
         index_nodes=new ArrayList<>(adj.keySet());
         Scanner sc=new Scanner(System.in);
         for(int i=0;i<edges;++i){
-            System.out.println("Enter Source and destination to add an edge :");
+//            System.out.println("Enter Source and destination to add an edge :");
             int source=sc.nextInt();
             int destination= sc.nextInt();
-            addEdge(source,destination);
+            try {
+                addEdge(source,destination,t);
+
+            }catch (NullPointerException|IndexOutOfBoundsException e){
+                System.err.println("enter correct number of nodes between 0 to "+(vertices-1));
+                System.exit(-1);
+            }
         }
     }
 
-    private void addEdge(int source,int destination){
+    private void transposeUtil(ON type){
+        if(type==ON.TRANSPOSE) {
+
+            for (int i = 0; i < index_nodes_transpose.size(); i++) {
+                for (int j = 0; j < adj.get(new Node(i)).size(); j++) {
+                    transpose.get(adj.get(index_nodes.get(i)).get(j)).add(index_nodes_transpose.get(i));  //copying values from adjacecny
+                    // list to tranpose adjaceny list ,,,we do this by forming opposite links
+                    //making sure nodes in linked list of transposed adj list as those same objects from key set of hasmap
+                }
+            }
+        }
+    }
+
+    private void addEdge(int source,int destination,TYPE t) throws NullPointerException,IndexOutOfBoundsException{
 
         Node s;
         Node d;
 
         d=index_nodes.get(destination);
         s=index_nodes.get(source);
+if(t==TYPE.DIRECTED){
 
+    adj.get(s).add(d);   //directed graph
+}
+else {
 
-    try {
         adj.get(s).add(d);
-        adj.get(d).add(s);
-    }
-    catch (NullPointerException e){
-        System.err.println("enter a valid node number in range of 0 to "+(adj.size()-1));
-        System.exit(1);
-    }
+        adj.get(d).add(s); //else undirected
 
+
+}
     }
 
     public void bfs(int source){
 
     Node current;
-
 
         Queue<Node> q=new LinkedList<>();
 
@@ -118,43 +149,96 @@ public class Graph {
     }
 
 
-    private void dfsUtil(Node source){
+    private void dfsUtil(Node source,HashMap<Node,LinkedList<Node>> adj,ON type){
         time++;
         source.startTime=time;      //helps in studying strongly connected components
         source.visited=true;
 
         for(Node v:adj.get(source)){
-            if(!v.visited){
-                                //dfs visited each and every node in graph weather it is a forest graph
-                v.predecessor=source;
-                dfsUtil(v);
+            if(!v.visited) {
+                //dfs visited each and every node in graph weather it is a forest graph
+                v.predecessor = source;
 
+                System.out.print("->" + v.node_no);
+
+
+                dfsUtil(v, adj, type);
             }
-            else if(v.visited && source.predecessor!=v){  //checking if graph has cycle and setting cycle variable of graph to true or false
+            else if(v.visited && source.predecessor!=v && type==ON.SIMPLE){  //checking if graph has cycle and setting cycle variable of graph to true or false
                 cycle=true;   //if the current source node has v in adjacency list and it is already visited and it is not parent of source node
                 //then it is a backedge which meaans graph has a cycle
             }
         }
+            if(type==ON.SIMPLE) {
+                transpose_stack.push(index_nodes_transpose.get(source.node_no));
+            //after a node finishes recursion push it onto the stack for tracking which has least or most time
+                //and then we will perform dfs on transpose of graph for decreasing finishing time
+            }
+
+
         time++;
         source.finishTime=time;
     }
 
-    public void dfs(){
-      for(Node n:index_nodes){
-          if(!n.visited){
-              dfsUtil(n);
-          }
-      }
+    public void dfs(ON adj_list){
+
+        if(adj_list==ON.SIMPLE) {
+
+            for (Node n : index_nodes) {
+
+                if (!n.visited) {
+                     //making sure by pushing index_nodes_transpose nodes that
+                    //that old visited node of old adj list should not cause problem as its a new node
+                    dfsUtil(n, this.adj,ON.SIMPLE);
+
+                }
+            }
+System.out.println();
+        }
+        else if(adj_list==ON.TRANSPOSE){
+            time=0;
+
+            while(!transpose_stack.isEmpty()) {
+                Node n=transpose_stack.poll();
+                if (!n.visited) {
+
+                    System.out.println("COMPONENT TREE: "); //it will print the forest formed by kosarajus algorithm
+                    System.out.print("->"+n.node_no);
+
+                    dfsUtil(n,transpose,ON.TRANSPOSE);
+                    System.out.println();
+
+                }
+            }
+
+        }
     }
 
-    public boolean hasCycle()
+    public void transpose(){
+     transposeUtil(ON.TRANSPOSE);
+    }
+
+    private void create_transpore_adj(){
+        transpose = new HashMap<>();
+        for (int i = 0; i < index_nodes.size(); i++) {
+            transpose.put(new Node(i), new LinkedList<>());
+        }
+        index_nodes_transpose = new ArrayList<>(transpose.keySet());
+    }
+    public void stronglyConnected(){
+        create_transpore_adj();
+        dfs(ON.SIMPLE);
+        transpose();
+        dfs(ON.TRANSPOSE);
+    }
+
+    public boolean hasCycle(ON adj_list)
     {
-        dfs();
+        dfs(adj_list);
         return cycle;
     }
     public boolean dfsStack(int source ,int destination){
         Node s=index_nodes.get(source);
-        Node d=index_nodes.get(destination);
         s.visited=true;
         Stack<Node> stk=new Stack<>();
         stk.push(s);
@@ -174,17 +258,15 @@ public class Graph {
     }
 
     public void printGraph(int source,int destination){
-        Node s=index_nodes.get(source);
+        //run dfs before printing this
         Node d=index_nodes.get(destination);
-        if(s.equals(d)){
-//            System.out.println(source);
-        }else if(d.predecessor==null){
-            System.out.println("No path found");
+        if(d.predecessor==null){
+
         }else{
             printGraph(source,d.predecessor.node_no);
             System.out.print("->"+d.node_no);
         }
-        System.out.println("");
+        System.out.println();
     }
 
 }
--- untitled/src/Bear.java
+++ untitled/src/Bear.java
@@ -2,13 +2,13 @@ import graphs.Graph;
 
 public class Bear {
     public static void main(String[] args){
-Graph g =new Graph(4,4);
+Graph g =new Graph(8,14, Graph.TYPE.DIRECTED);
 //g.bfs(0);
 //g.printGraph(0,3);
 //System.out.print(g.dfs(0,2));
 //System.out.println(g.dfsStack(0,2));
+        g.stronglyConnected();
 
-System.out.println(g.hasCycle());
 
     }
 }
